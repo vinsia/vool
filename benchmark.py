815459b39ef4dbfa336206347742577e23a0b3bb
VinSia <VinSia@live.cn>
2018-07-11 11:53:49 +0800

    修改评测方法

--- benchmark.py
+++ benchmark.py
@@ -4,9 +4,6 @@ from __future__ import unicode_literals
 import os
 import sys
 import time
-from multiprocessing import Process
-
-from wsgi import WSGIServer
 
 
 def daemonize() -> None:
@@ -39,33 +36,11 @@ def _measure():
     start = time.time()
     import requests
     while True:
-        response = requests.get("http://127.0.0.1:9000")
+        response = requests.get("http://47.95.5.22:9000")
         count += 1
-        if count % 1000 == 0:
+        if count % 100 == 0:
             print(count, time.time() - start)
 
 
 if __name__ == "__main__":
-    from flask import Flask
-    from flask import Response
-
-    flask_app = Flask(__name__)
-
-
-    @flask_app.route('/hello')
-    def hello_world():
-        return Response(
-            'hello',
-            mimetype='text/plain'
-        )
-
-
-    server = WSGIServer(("0.0.0.0", 9000), flask_app.wsgi_app)
-    s_p = Process(target=server.start, args=())
-    s_p.start()
-
-    c_p = Process(target=_measure, args=())
-    c_p.start()
-
-    s_p.join()
-    c_p.join()
+    _measure()
