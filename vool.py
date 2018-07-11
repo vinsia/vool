8c036466ff1410da0228a38a4ffe0f6195767f1a
VinSia <VinSia@live.cn>
2018-07-11 17:46:23 +0800

    添加路由功能

---
+++ vool.py
@@ -0,0 +1,91 @@
+# -*- coding:utf-8 -*-
+from __future__ import unicode_literals
+
+import asyncio
+import re
+
+import uvloop
+
+from wsgi import WSGIServer
+
+
+class Route(object):
+    _route = None
+    simple = {}
+    complex = {}
+
+    def __new__(cls, *args, **kwargs):
+        if cls._route is None:
+            cls._route = super().__new__(cls)
+        return cls._route
+
+    def get_handler(self, path, method):
+        if path in self.simple.get(method, {}):
+            return self.simple[method][path], {}
+
+        print(self.complex)
+        for pattern, handler in self.complex.get(method, []):
+            match = pattern.match(path)
+            if match:
+                return handler, match.groupdict()
+
+    def compile_route(self, path):
+        path = re.sub(r"\{(\w+)\}", r'(?P<\1>\w+)', path)
+        return re.compile('^%s$' % path)
+
+    def add_route(self, path, method, handler):
+        # 去掉左边的/
+        match = re.match(r"^/(\w+/)*\w+$", path)
+        if match:
+            self.simple.setdefault(method, {})[path] = handler
+        else:
+            match = self.compile_route(path)
+            self.complex.setdefault(method, []).append((match, handler))
+
+
+def route(path, method="GET"):
+    """
+    /index
+    /{id}/{abc}
+    :param method:
+    :param path:
+    :return:
+    """
+
+    def decorator(fn):
+        r = Route()
+        r.add_route(path, method, fn)
+
+    return decorator
+
+
+@route("/hello")
+def hello():
+    return 200, "hello"
+
+
+@route("/hello/{name}")
+def hello_id(name):
+    return 200, "hello %s" % (name)
+
+
+class Vool:
+    route_manager = Route()
+
+    def application(self, environ, start_response):
+        path = environ.get("PATH_INFO")
+        method = environ.get("REQUEST_METHOD")
+        handler, kwargs = self.route_manager.get_handler(path, method)
+        status, response = handler(**kwargs)
+        start_response(status, {})
+        return iter([response.encode("utf-8")])
+
+
+if __name__ == "__main__":
+    vool = Vool()
+    asyncio.set_event_loop_policy(uvloop.EventLoopPolicy())
+    loop = asyncio.get_event_loop()
+    server = WSGIServer(vool.application)
+    f = asyncio.start_server(server.accept_client, port=9000, loop=loop)
+    server = loop.run_until_complete(f)
+    loop.run_forever()
--- wsgi.py
+++ wsgi.py
@@ -2,115 +2,97 @@
 from __future__ import unicode_literals
 
 import asyncio
-import socket
 import sys
 from io import StringIO
-from wsgiref.validate import validator
 
-BUFFER_SIZE = 4096
+import uvloop
 
 
-class WSGIServer(object):
-    """
-    :param address = ("127.0.0.1", 8080)
-    """
-    server_name = "localhost"
-    server_port = "80"
-
-    def __init__(self, address, application):
-        self.loop = None
-        self.application = application
-        self._socket = sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-        sock.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
-        sock.bind(address)
-        sock.listen(1)
-
-        host, port = sock.getsockname()[:2]
-        WSGIServer.server_name = socket.getfqdn(host)
-        WSGIServer.server_port = str(port)
-        self.status = 200
+class Response(object):
+    def __init__(self):
+        self.status = None
         self.response_headers = []
-
-    async def serve_forever(self):
-        loop = self.loop
-        while True:
-            client_connection, _ = await loop.sock_accept(self._socket)
-            await self.handle_request(client_connection)
-            # await loop.create_task(self.handle_request(client_connection))
-
-    async def handle_request(self, client_connection):
-        raw_data = await self.loop.sock_recv(client_connection, BUFFER_SIZE)  # TODO: 要全部输入
-        environ = self.parse_data(raw_data)
-        result = self.application(environ, self.start_response)
-        await self.finish_response(client_connection, result)
+        self.result = None
 
     def start_response(self, status, response_headers, exc_info=None):
         self.status = status
         self.response_headers = response_headers
 
-    async def finish_response(self, client_connection, result):
+
+def parse_parameter(path):
+    if "?" not in path:
+        return path, ""
+
+    path, parameter_str = path.split("?")
+    return path, parameter_str
+
+
+class WSGIServer(asyncio.Protocol):
+    server_name = "localhost"
+    server_port = "80"
+
+    def __init__(self, application):
+        self.application = application
+
+    async def accept_client(self, client_reader, client_writer):
+        try:
+            raw_data = await client_reader.read(1024)
+            data = raw_data.decode("utf-8")
+            format_data = data.splitlines()
+
+            # print(format_data)
+            # 第一行为 'method path version'
+            method, url, http_version = format_data[0].split() if format_data else ["GET", "/", "http 1.0"]
+            path, query_str = parse_parameter(url)
+            # https://www.python.org/dev/peps/pep-0333/#environ-variables
+            environ = {
+                "REQUEST_METHOD": method,
+                "SCRIPT_NAME": "",
+                "PATH_INFO": path,
+                "QUERY_STRING": query_str,
+                "CONTENT_TYPE": "text/plain",
+                "SERVER_NAME": WSGIServer.server_name,
+                "SERVER_PORT": WSGIServer.server_port,
+                "SERVER_PROTOCOL": http_version,
+                "wsgi.version": (1, 0),
+                "wsgi.url_scheme": "http",
+                "wsgi.input": StringIO(data),
+                "wsgi.errors": sys.stderr,
+                "wsgi.multithread": False,
+                "wsgi.multiprocess": False,
+                "wsgi.run_once": False,
+            }
+            response = Response()
+            response.result = self.application(environ, response.start_response)
+            await self.finish_response(client_writer, response)
+        finally:
+            await client_writer.drain()
+            client_writer.close()
+
+    @staticmethod
+    async def finish_response(client_writer, response):
         try:
             # TODO: 是否有更好的处理bytes的方法
-            response = bytes('HTTP/1.1 {status}\r\n'.format(status=self.status), encoding="utf-8")
-            for header in self.response_headers:
-                response += bytes('{0}: {1}\r\n'.format(*header), encoding="utf-8")
-            response += bytes('\r\n', encoding="utf-8")
+            resp = bytes('HTTP/1.1 {status}\r\n'.format(status=response.status), encoding="utf-8")
+            for header in response.response_headers:
+                resp += bytes('{0}: {1}\r\n'.format(*header), encoding="utf-8")
+            resp += bytes('\r\n', encoding="utf-8")
             # result为一个迭代器，而不一定string或者byte
-            for data in result:
-                response += data
+            for data in response.result:
+                resp += data
             # 迭代器必须关闭？ 为啥?
-            result.close()
-            await self.loop.sock_sendall(client_connection, response)
+            # try:
+            #     response.result.close()
+            # except:
+            #     pass
+            client_writer.write(resp)
         except:
             import traceback
             traceback.print_exc()
-        finally:
-            client_connection.close()
-
-    def parse_data(self, raw_data):
-        data = raw_data.decode("utf-8")
-        format_data = data.splitlines()
-
-        # print(format_data)
-        # 第一行为 'method path version'
-        method, url, http_version = format_data[0].split()
-        path, query_str = WSGIServer.parse_parameter(url)
-        # https://www.python.org/dev/peps/pep-0333/#environ-variables
-        environ = {
-            "REQUEST_METHOD": method,
-            "SCRIPT_NAME": "",
-            "PATH_INFO": path,
-            "QUERY_STRING": query_str,
-            # "CONTENT_TYPE": "text/plain",
-            "SERVER_NAME": WSGIServer.server_name,
-            "SERVER_PORT": WSGIServer.server_port,
-            "SERVER_PROTOCOL": http_version,
-            "wsgi.version": (1, 0),
-            "wsgi.url_scheme": "http",
-            "wsgi.input": StringIO(data),
-            "wsgi.errors": sys.stderr,
-            "wsgi.multithread": False,
-            "wsgi.multiprocess": False,
-            "wsgi.run_once": False,
-        }
-        return environ
-
-    @staticmethod
-    def parse_parameter(path):
-        if "?" not in path:
-            return path, ""
-
-        path, parameter_str = path.split("?")
-        return path, parameter_str
-
-    def start(self):
-        loop = asyncio.get_event_loop()
-        self.loop = loop
-        loop.run_until_complete(self.serve_forever())
 
 
 if __name__ == "__main__":
-    @validator
+    # @validator
     def application(environ, start_response):
         data = b'Hello, World!\n'
         status = '200 OK'
@@ -123,5 +105,9 @@ if __name__ == "__main__":
         return iter([data])
 
 
-    server = WSGIServer(("0.0.0.0", 9000), application)
-    server.start()
+    asyncio.set_event_loop_policy(uvloop.EventLoopPolicy())
+    loop = asyncio.get_event_loop()
+    server = WSGIServer(application)
+    f = asyncio.start_server(server.accept_client, port=9000, loop=loop)
+    server = loop.run_until_complete(f)
+    loop.run_forever()
